d45d82aa0a3bb8402d0e0bc2883fb8615f91fe6b
Jiyoung Yun <jy910.yun@samsung.com>
2015-01-16 18:59:38 +0900

    device-node: Add HAL common and backlight structures
    Each device shuld be contained struct hw_info_t module as a first variable.
    It is a common interface among multiple device HAL structures.
    
    Backlight HAL structure is for controlling the brightness of backlight.
    You can set the birhgntess value using it and it also support to sensor mode.
    It makes backlight display to be changed by light sensor value.
    
    Developers can make their own HAL structure.
    In this case, it should be located in $LIB_INSTALL_DIR/hw/external/.
    And user can use default HAL structure in $LIB_INSTALL_DIR/hw/default/.
    
    There is an detail information in Tizen Wiki:
    https://wiki.tizen.org/wiki/HAL_upgrade_for_Tizen_3.0#Device
    
    Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>
    Change-Id: Id73f5517e659a652a634cfa86c3137f5aca859c7

---
+++ hw/default/backlight.c
@@ -0,0 +1,60 @@
+/*
+ * device-node
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <stdio.h>
+#include <errno.h>
+
+#include <hw/backlight.h>
+#include "device-internal.h"
+
+static int backlight_set_state(struct backlight_state_t *state)
+{
+	/* TODO: Need to implement */
+	return 0;
+}
+
+static int backlight_open(struct hw_info_t *info,
+		struct hw_common_t **common)
+{
+	struct backlight_device_t *backlight_dev;
+
+	if (!info || !*common)
+		return -EINVAL;
+
+	backlight_dev = calloc(1, sizeof(struct backlight_device_t));
+	if (!backlight_dev)
+		return -ENOMEM;
+
+	backlight_dev->common.info = info;
+	backlight_dev->set_state = backlight_set_state;
+
+	*common = (struct hw_common_t*)backlight_dev;
+
+	return 0;
+}
+
+struct hw_info_t HARDWARE_INFO_SYM = {
+	.unique_id = HARDWARE_INFO_TAG,
+	.hal_version = HARDWARE_INFO_VERSION,
+	.device_version = BACKLIGHT_HARDWARE_DEVICE_VERSION,
+	.id = BACKLIGHT_HARDWARE_DEVICE_ID,
+	.name = "Default Backlight",
+	.author = "Jiyoung Yun <jy910.yun@samsung.com>",
+	.open = backlight_open,
+};
