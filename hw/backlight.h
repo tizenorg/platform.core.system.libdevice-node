8f2f80aa16b18da410157304e2a7c9651308c1d8
Jiyoung Yun <jy910.yun@samsung.com>
2015-01-16 18:59:38 +0900

    device-node: Add HAL common and backlight structures
    Each device shuld be contained struct hw_info_t module as a first variable.
    It is a common interface among multiple device HAL structures.
    
    Backlight HAL structure is for controlling the brightness of backlight.
    You can set the birhgntess value using it and it also support to sensor mode.
    It makes backlight display to be changed by light sensor value.
    
    Developers can make their own HAL structure.
    In this case, it should be located in $LIB_INSTALL_DIR/hw/external/.
    And user can use default HAL structure in $LIB_INSTALL_DIR/hw/default/.
    
    There is an detail information in Tizen Wiki:
    https://wiki.tizen.org/wiki/HAL_upgrade_for_Tizen_3.0#Device
    
    Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>
    Change-Id: Id73f5517e659a652a634cfa86c3137f5aca859c7

---
+++ hw/backlight.h
@@ -0,0 +1,59 @@
+/*
+ * libdevice-node
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __HW_BACKLIGHT_H__
+#define __HW_BACKLIGHT_H__
+
+#include <hw/common.h>
+
+/**
+ * The id of this device
+ */
+#define BACKLIGHT_HARDWARE_DEVICE_ID	"backlight"
+
+/**
+ * The version of this device
+ */
+#define BACKLIGHT_HARDWARE_DEVICE_VERSION	MAKE_VERSION(1,0)
+
+/**
+ * The mode of backlight
+ */
+enum backlight_mode {
+	BACKLIGHT_MANUAL,	/* Manual setting */
+	BACKLIGHT_SENSOR,	/* Worked by sensor */
+};
+
+/**
+ * The state of this device
+ */
+struct backlight_state_t {
+	/** The range of brightness is 0 to 100 */
+	int brightness;
+	/** refer to enum lights_mode */
+	enum backlight_mode mode;
+};
+
+struct backlight_device_t {
+	struct hw_common_t common;
+	
+	int (*set_state)(struct backlight_state_t *state);
+};
+
+#endif
