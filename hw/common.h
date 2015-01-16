5e696b6e217f7c8285e149b3f5b9b712f9bfcab8
Jiyoung Yun <jy910.yun@samsung.com>
2015-01-16 18:59:38 +0900

    device-node: Add HAL common and backlight structures
    Each device shuld be contained struct hw_info_t module as a first variable.
    It is a common interface among multiple device HAL structures.
    
    Backlight HAL structure is for controlling the brightness of backlight.
    You can set the birhgntess value using it and it also support to sensor mode.
    It makes backlight display to be changed by light sensor value.
    
    Developers can make their own HAL structure.
    In this case, it should be located in $LIB_INSTALL_DIR/hw/external/.
    And user can use default HAL structure in $LIB_INSTALL_DIR/hw/default/.
    
    There is an detail information in Tizen Wiki:
    https://wiki.tizen.org/wiki/HAL_upgrade_for_Tizen_3.0#Device
    
    Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>
    Change-Id: Id73f5517e659a652a634cfa86c3137f5aca859c7

---
+++ hw/common.h
@@ -0,0 +1,78 @@
+/*
+ * libdevice-node
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __HW_COMMON_H__
+#define __HW_COMMON_H__
+
+#include <stdint.h>
+
+#define MAKE_TAG_CONSTANT(A,B,C,D)	(((A) << 24) | ((B) << 16) | ((C) << 8) | (D))
+#define HARDWARE_INFO_TAG	MAKE_TAG_CONSTANT('T','H','I','T')
+
+struct hw_common_t;
+struct hw_info_t {
+	/* unique_id must be initialized to HARDWARE_INFO_TAG */
+	uint32_t unique_id;
+	/* HAL version */
+	uint16_t hal_version;
+	/* device version */
+	uint16_t device_version;
+	/* device id */
+	const char *id;
+	/* device name */
+	const char *name;
+	/* author name */
+	const char *author;
+	/* module's dso */
+	void *dso;
+	/* reserved for future use */
+	uint32_t reserved[8];
+	/* open device */
+	int (*open)(struct hw_info_t *info,
+			const char *id, struct hw_common_t **common);
+	/* close device */
+	int (*close)(struct hw_common_t *common);
+};
+
+struct hw_common_t {
+	/* indicate to this device information structure */
+	struct hw_info_t *info;
+};
+
+#define MAKE_VERSION(maj,min) \
+	((((maj) & 0xff) << 8) | ((min) & 0xff))
+
+/**
+ * Version of the struct hw_info_t
+ */
+#define HARDWARE_INFO_VERSION	MAKE_VERSION(1,0)
+
+/**
+ * Name of the hardware info symbolic (Tizen Hardware Info)
+ */
+#define HARDWARE_INFO_SYM		TizenHwInfo
+
+/**
+ * Name of the hardware info symbolic as a string
+ */
+#define HARDWARE_INFO_SYM_AS_STR	"TizenHwInfo"
+
+int hw_get_info(const char *id, const struct hw_info_t **info);
+
+#endif
---
+++ hw/default/shared.h
@@ -0,0 +1,32 @@
+/*
+ * libdevice-node
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __HW_DEFAULT_SHARED_H__
+#define __HW_DEFAULT_SHARED_H__
+
+#ifndef EXPORT
+#define EXPORT	__attribute__ ((visibility("default")))
+#endif
+
+int sys_get_int(char *fname, int *val);
+int sys_get_str(char *fname, char str[]);
+int sys_set_int(char *fname, int val);
+int sys_set_str(char *fname, char *val);
+
+#endif
