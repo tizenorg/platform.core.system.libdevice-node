a07f8ef5df840aaae98afb2434e39919f14f3712
Jiyoung Yun <jy910.yun@samsung.com>
2015-01-16 18:59:38 +0900

    device-node: Add OAL common and lights structures
    OAL redesign wiki:
    https://wiki.tizen.org/wiki/OAL_upgrade_for_Tizen_3.0#Device
    
    Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>
    Change-Id: Id73f5517e659a652a634cfa86c3137f5aca859c7

---
+++ oal/common.h
@@ -0,0 +1,55 @@
+/*
+ * deviced
+ *
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __OAL_COMMON_H__
+#define __OAL_COMMON_H__
+
+#define MAKE_TAG_CONSTANT(A,B,C,D)	(((A) << 24) | ((B) << 16) | ((C) << 8) | (D))
+#define TIZEN_HARDWARE_DEVICE_TAG	MAKE_TAG_CONSTANT('T','H','D','T')
+
+struct hw_device_t {
+	/* unique_id must be initialized to TIZEN_HARDWARE_DEVICE_TAG */
+	uint32_t uniqe_id;
+	/* device OAL version */
+	uint16_t device_oal_version;
+	/* device api version */
+	uint16_t device_api_version;
+	/* device name */
+	const char *name;
+	/* module's dso */
+	void *dso;
+	/* reserved for future use */
+	uint32_t reserved[4];
+	/* open device */
+	int (*open)(struct hw_device_t *device);
+};
+
+/**
+ * Name of the oal_device_info (Tizen Oal Device)
+ */
+#define OAL_DEVICE_INFO_SYM		TOD
+
+/**
+ * Name of the oal_device_info as a string
+ */
+#define OAL_DEVICE_INFO_SYM_AS_STR	"TOD"
+
+int hw_get_module(const char *id, const struct hw_device_t **device);
+
+#endif
---
+++ oal/lights.h
@@ -0,0 +1,54 @@
+/*
+ * deviced
+ *
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __OAL_LIGHTS_H__
+#define __OAL_LIGHTS_H__
+
+#include <hardware/common.h>
+
+/**
+ * The id of this device
+ */
+#define LIGHTS_HARDWARE_DEVICE_ID	"lights"
+
+/**
+ * The mode of lights
+ */
+enum lights_mode {
+	LIGHTS_MANUAL,	/* Manual setting */
+	LIGHTS_SENSOR,	/* Worked by sensor */
+};
+
+/**
+ * The state of this device
+ */
+struct lights_state_t {
+	/** The range of brightness is 0 to 100 */
+	int brightness;
+	/** refer to enum lights_mode */
+	enum lights_mode mode;
+};
+
+struct lights_device_t {
+	struct hw_device_t common;
+	
+	int (*set_state)(struct lights_state_t *state);
+};
+
+#endif
