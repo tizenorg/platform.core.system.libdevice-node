8b9ca0aaf47d2e766c5d811a11288076fb81e33c
Jiyoung Yun <jy910.yun@samsung.com>
2015-02-27 19:51:46 +0900

    device-node: Add display device structure
    Backlight structure will replace with display structure.
    The role and mean of backlight is too restrictive.
    Display will cover all function of drm, backlight and so on.
    
    Change-Id: Icf0a45d2fb522f0f55b7210fabf4a8b5bd77ef3b
    Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

---
+++ hw/display.h
@@ -0,0 +1,56 @@
+/*
+ * libdevice-node
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __HW_DISPLAY_H__
+#define __HW_DISPLAY_H__
+
+#include <hw/common.h>
+
+/**
+ * The id of this device
+ */
+#define DISPLAY_HARDWARE_DEVICE_ID	"display"
+
+/**
+ * The version of this device
+ */
+#define DISPLAY_HARDWARE_DEVICE_VERSION	MAKE_VERSION(1,0)
+
+enum display_power {
+	DISPLAY_POWER_ON,		/* In use */
+	DISPLAY_POWER_STANDBY,	/* Blanked, low power */
+	DISPLAY_POWER_SUSPEND,	/* Blanked, lower power */
+	DISPLAY_POWER_OFF,		/* Shut off, awaiting activity */
+};
+
+struct display_device {
+	struct hw_common common;
+
+	/**
+	 * The brightness value is 0 to 100.
+	 */
+	int (*get_brightness)(int *brightness);
+	int (*set_brightness)(int brightness);
+
+	/* Control display power */
+	int (*get_power_state)(enum display_power *state);
+	int (*set_power_state)(enum display_power state);
+};
+
+#endif
