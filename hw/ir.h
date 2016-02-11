1840cb2f4ff8113b5cca440a152573dd090882b6
pr.jung <pr.jung@samsung.com>
2016-02-11 16:46:28 +0900

    ir: Add HAL interface
    - Get information whether ir is available.
    - Transmit ir pattern.
    
    Change-Id: If3196bbbe39452917c76b8f3a74e81d94dec94c7
    Signed-off-by: pr.jung <pr.jung@samsung.com>

---
+++ hw/ir.h
@@ -0,0 +1,43 @@
+/*
+ * libdevice-node
+ *
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __HW_IR_H__
+#define __HW_IR_H__
+
+#include <hw/common.h>
+
+/**
+ * The id of this device
+ */
+#define IR_HARDWARE_DEVICE_ID	"ir"
+
+/**
+ * The version of this device
+ */
+#define IR_HARDWARE_DEVICE_VERSION	MAKE_VERSION(0,1)
+
+struct ir_device {
+	struct hw_common common;
+
+	/* Control ir state */
+	int (*is_available)(bool *available);
+	int (*transmit)(int *frequency_pattern, int size);
+};
+
+#endif
